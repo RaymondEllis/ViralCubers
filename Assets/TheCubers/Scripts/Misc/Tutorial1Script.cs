b61bff4cc2b3491db4e6779da6876eb0a60bc746
Raymond Ellis <raymondellis89@gmail.com>
2015-08-05 16:42:10 -0700

    joystick help

---
+++ Assets/TheCubers/Scripts/Misc/Tutorial1Script.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace TheCubers
+{
+	public class Tutorial1Script : MonoBehaviour
+	{
+		public Transform JoyCam;
+		public Transform JoyCamRot;
+
+		public float StickMove;
+
+		private Quaternion joyCamRest;
+		private Quaternion joyCamRotRest;
+
+		void Start()
+		{
+			if (JoyCam)
+				joyCamRest = JoyCam.localRotation;
+			if (JoyCamRot)
+				joyCamRotRest = JoyCamRot.localRotation;
+		}
+
+		void Update()
+		{
+			if (JoyCam)
+				JoyCam.localRotation = joyCamRest * Quaternion.Euler(MyInput.Axis(Inp.CameraVertical) * StickMove, -MyInput.Axis(Inp.CameraHorizontal) * StickMove, 0f);
+			if (JoyCamRot)
+				JoyCamRot.localRotation = joyCamRotRest * Quaternion.Euler(0f, -MyInput.Axis(Inp.CameraRotate) * StickMove, 0f);
+		}
+	}
+}
--- Assets/TheCubers/Scripts/ScoreInfo.cs
+++ Assets/TheCubers/Scripts/ScoreInfo.cs
@@ -32,11 +32,14 @@ namespace TheCubers
 					Debug.LogError("Did not find level info? " + level);
 					break;
 				case 1: // more
-					return string.Format("You needed {0:0} more points to finsh.", info.GoalScore - score);
+					return string.Format("You needed {0:0} or more points to finish.", info.GoalScore - score);
 				case 2: // equal
-					return string.Format("You needed exactly {0:0} points to finsh.", info.GoalScore);
+					if (score > info.GoalScore)
+						return string.Format("You needed exactly {0:0} fewer points to finish.", info.GoalScore);
+					else
+						return string.Format("You needed exactly {0:0} more points to finish.", info.GoalScore);
 				case 3: // less
-					return string.Format("You needed {0:0} fewer points to finsh.", score - info.GoalScore);
+					return string.Format("You needed {0:0} fewer points to finish.", score - info.GoalScore);
 			}
 			return "Unknown goal";
 		}
